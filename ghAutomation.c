3ec5a0857937937f4482885e8e65b2cdcfd7ef0d
Ronit Vairagi <17uec101@lnmiit.ac.in>
2020-10-03 14:55:57 +0530

    build ready initial code

--- ghAutomation.c
+++ ghAutomation.c
@@ -1,8 +1,14 @@
-/*
- * ghAutomation.c
+/**/
+
+#include "ghAutomation.h"
+
+/**********************************************************************************************************************
+ * @function	-	SetupAutomation()
  *
- *  Created on: 03-Oct-2020
- *      Author: ronit
- */
-
-
+ * @description	-	This API calls all the configuration functions used for GPIO and other automation operations/config
+ * 					used in our project.
+ *********************************************************************************************************************/
+void SetupAutomation(void)
+{
+	//
+}
--- main.c
+++ main.c
@@ -1,19 +1,111 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
+/* Write file and brief project description*/
+#include "ghRTOS.h"
+#include "ghAutomation.h"
+			
+/* ********************************************** Flags & Misc. Alias ********************************************** */
+#define CMD_ON		0x01
+#define CMD_OFF		0x00
+
+#define Auto		True
+#define Manual		False
+
+#define Active		True
+#define NotActive	False
+
+/* ************************************************* Task Handles ************************************************** */
+TaskHandle_t xTH_Test;
+// Continuous
+/*TaskHandle_t xCT1_H_SyncWithServer;
 */
 
+// Occasional
+/*TaskHandle_t xOT1_H_FanSpeedControl;
+TaskHandle_t xOT2_H_ApplianceControl;
+*/
 
-#include "ghRTOS.h"
-			
+// Rare -> Event Triggered
+/*TaskHandle_t xRT1_H_Doorbell;
+TaskHandle_t xRT2_H_StaircaseLights;
+TaskHandle_t xRT3_H_NightLight;
+TaskHandle_t xRT4_H_Curtains;
+*/
 
+/* ********************************************* Interrupt Functions *********************************************** */
+void FireAndTheftAlarm(void);
+void vToggleAppliance( uint8_t ApplianceID, BaseType_t* pxHigherPriorityTaskWoken);
+
+
+/* ************************************************ Task Functions ************************************************* */
+void vT_Test(void* param);/*
+// Continuous
+void vCT1_SyncWithServer(void* param);
+
+// Occasional
+void vOT1_FanSpeedControl(void* param);
+void vOT2_ApplianceControl(void* param);
+
+// Rare -> Event Triggered
+void vRT1_Doorbell(void* param);
+void vRT2_StaircaseLights(void* param);
+void vRT3_NightLight(void* param);
+void vRT4_Curtains(void* param);
+*/
+
+/* ************************************************ User Functions ************************************************* */
+void blinkLED(void);
+
+
+/* ************************************************** User Types *************************************************** */
+
+
+/* ************************************************* Global Space ************************************************** */
+char message[250];
+
+
+
+/* ***************************************************** main ****************************************************** */
 int main(void)
 {
+	SetupHardware();
+	SetupAutomation();
 
-	for(;;);
+	blinkLED();
+
+	// Create Tasks
+
+	// Create Queues
+
+	// Start Scheduler
+	printMsg("\n\r Starting scheduler... \n\r");
+	vTaskStartScheduler();
+
+	while(1);
 }
+
+
+
+/* ******************************************** Task Function Definition ******************************************* */
+
+
+
+
+/* ******************************************* User Functions' Definition ****************************************** */
+void blinkLED(void)
+{
+	for(int i = 0; i<6; i++)
+	{
+		GPIO_ToggleBits(GPIOA, GPIO_Pin_5);
+		for(int j = 0; j<500000; j++)
+		{
+			// wait
+		}
+	}
+}
+
+
+
+/* ********************************************** Interrupt Handlers *********************************************** */
+
+
+
+/* ****************************************** Interrupt Helper Functions ******************************************* */
